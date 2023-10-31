ec50f86e77a2dc4d2ad036cab7d093e8e8f0e73f
precious-appiah <appiahprecious99@gmal.com>
2023-10-30 18:14:38 -0700

    fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-
 print(f"object-oriented programming with Python")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+url='https://peps.python.org/pep-0020/#the-zen-of-python"
+print("The Zen of Python, by Tim Peters")
