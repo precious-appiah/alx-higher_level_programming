8e0ac54b4fe976cfcd0c901bd7e2d24ba745da08
precious-appiah <appiahprecious99@gmail.com>
2023-10-31 17:53:51 -0700

    fix for 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    #print("{}".format(number % 10), end='')
+def print_last_digit(number): 
     print(f"{number % 10}", end='')
     return number % 10
