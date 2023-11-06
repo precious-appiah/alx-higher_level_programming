5d56a26c9dec518c1c1452df3bc87363af35d328
precious-appiah <appiahprecious99@gmail.com>
2023-11-06 09:19:00 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ''
     for i in string:
-        if i == 'c' or i == 'C':
-            str
+        if i != 'c' or i != 'C':
+            new_str = new_str + i
+    return new_str
