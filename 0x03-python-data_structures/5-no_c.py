55e43e435be7e31ae93219e653ee6bc82e056af7
precious-appiah <appiahprecious99@gmail.com>
2023-11-06 09:20:09 -0800

    5-fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ''
     for i in string:
-        if i != 'c' or i != 'C':
+        if i != 'c' and i != 'C':
             new_str = new_str + i
     return new_str
