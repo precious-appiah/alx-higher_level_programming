0aad0c4752e593feedaf12419a7f3c1db10ed345
precious-appiah <appiahprecious99@gmail.com>
2023-11-06 08:51:22 -0800

    4-refactor

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list_cp = []
-    for i in range(0, len(my_list)):
-        my_list_cp.append(my_list[i])
+    my_list_cp = my_list.copy
     if idx < 0 or idx >= len(my_list):
         return my_list_cp
     my_list_cp[idx] = element
