999753d0f0f8ecbc950d082e24311d2200b71ecc
precious-appiah <appiahprecious99@gmail.com>
2023-11-06 06:51:06 -0800

    4-fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list_cp = my_list
+    my_list_cp = []
+    for i in range(0, len(my_list)):
+        my_list_cp[i] = my_list[i]
     if idx < 0 or idx >= len(my_list):
         return my_list_cp
     my_list_cp[idx] = element
