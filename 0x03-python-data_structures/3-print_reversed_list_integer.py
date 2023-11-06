850cf2009c84a5c2e2f400e882cc46498a45a9f5
precious-appiah <appiahprecious99@gmail.com>
2023-11-06 08:59:47 -0800

    3-reverse refactor

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list.reverse()
-    for i in new_list:
+    for i in my_list.reverse():
         print("{:d}".format(i))
