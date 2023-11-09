13984239419d26dd7100572d7e0fc91af6174893
precious-appiah <appiahprecious99@gmail.com>
2023-11-09 12:17:07 -0800

    0-fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     for i in matrix:
         for j in i:
-            matrix[i][j] *= matrix[i][j]
+            matrix[i][j] = matrix[i][j] ** 2
     return matrix
