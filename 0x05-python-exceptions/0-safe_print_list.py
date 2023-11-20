555e6b48158ac43094f8feea975f56d39fe0a5b4
precious-appiah <appiahprecious99@gmail.com>
2023-11-20 07:37:53 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0): 
+def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             if type(my_list[i]) in [int, float]:
