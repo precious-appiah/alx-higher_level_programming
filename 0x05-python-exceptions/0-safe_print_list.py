32f31ddef7194b8196790a94076c03da1a74850b
precious-appiah <appiahprecious99@gmail.com>
2023-11-20 09:59:10 -0800

    task 0 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,8 @@ def safe_print_list(my_list=[], x=0):
             cnt += 1
             print("")
     except IndexError:
-        print("Not enough elemnets in list")
+        print("")
+        return cnt
     except Exception:
         print("An Error occured")
     return cnt
