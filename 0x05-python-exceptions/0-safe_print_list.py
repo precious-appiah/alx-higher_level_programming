8af120d353af226041fc1cd8759685ae14b5e0d4
precious-appiah <appiahprecious99@gmail.com>
2023-11-20 07:21:45 -0800

    init and task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0): 
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        print("Not enough elemnets in list")
+    except Exception:
+        print("An Error occured")
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Learniong Exceptions
