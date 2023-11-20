b869c01b3c74e3a2a004dd6f5bfeb45492b749d3
precious-appiah <appiahprecious99@gmail.com>
2023-11-20 15:22:41 -0800

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as err:
+        print("Exception: {}".format(err))
