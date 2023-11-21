ea50a8e2dda0d9682edbca3d8c4e9f0f46a756b8
precious-appiah <appiahprecious99@gmail.com>
2023-11-21 13:40:45 -0800

    task 1wq

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to define a square class
+    Private instance variable size
+    instantiation with size
+    """
+
+
+class Square:
+
+    """This is a class doc describong what a square is"""
+
+    def __init__(self, size):
+
+        """this is an init
+            size is the private variable
+            """
+        self.size = size
