ebf4d2f71084812747d8475be88bbaadb5fce3bb
precious-appiah <appiahprecious99@gmail.com>
2023-11-21 15:12:38 -0800

    task 4 fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,8 +30,10 @@ class Square:
     def size(self, value):
         if not isinstance(value, it):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """public instance method
