0c16d0dd08a93389317004f0e667b68a440da274
precious-appiah <appiahprecious99@gmail.com>
2023-11-21 15:08:17 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module to define a square class
+    Private instance variable size
+    instantiation with size
+    """
+
+
+class Square:
+
+    """This is a class doc describong what a square is"""
+
+    def __init__(self, size=0):
+
+        """this is an init
+            size is the private variable
+            checks if size is an integer
+            check if size is >=0
+            """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, it):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """public instance method
+        that returns area of a square
+        """
+        return self.__size ** 2
