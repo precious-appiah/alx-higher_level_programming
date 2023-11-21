617171f583416e7667aacc5bec42eb3acc3cd40f
precious-appiah <appiahprecious99@gmail.com>
2023-11-20 16:06:16 -0800

    task 4 fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,23 +3,22 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     i = 0
     try:
-        for j in my_list_1:
-            for k in my_list_2:
-                if isinstance(j, (int, float)) or if isinstance(k, (int, float)):
+        for j, k in zip(my_list_1, my_list_2):
+            try:
+                if isinstance(j, (int, float)) and  isinstance(k, (int, float)):
                     res = j / k
                     new_list.append(res)
-                    i++
                 else:
                     res = 0
                     new_list.append(res)
                     i++
-    except ZeroDivisionError:
-        new_list.append(0)
-        print("division by 0")
-        i++
-    except IndexError:
-        new_list.append(0)
-        print("out of range")
-        i++
+            except ZeroDivisionError:
+                new_list.append(0)
+                print("division by 0")
+            except IndexError:
+                new_list.append(0)
+                print("out of range")
+    except Exception as err:
+        print("Exception {}".format(err))
     finally:
         return new_list
