6fa9fb1e01b6257d35b2c8ff66238725c4dd278b
precious-appiah <appiahprecious99@gmail.com>
2023-11-21 13:24:45 -0800

    task 0 fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module to define a square class"""
+
+
 class Square:
+
     """This is a class doc describong what a square is"""
-    def __init__(self):
-        """This is the init fucntion that initialises params"""
+
     pass
