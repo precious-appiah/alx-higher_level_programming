2903a97fb14f63bc8c8a8e8a0a1934ad1733ff9f
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 15:19:48 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""modulet to define an empty class"""
+
+
+class BaseGeometry:
+
+    """defines an empty class"""
+
+    def area(self):
+
+        """function to raise an exception"""
+
+        raise Exception("area() is not implemented")
