ec47744a176eb8f1018c2dc9db5d2fb875074b62
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 14:46:00 -0800

    task 2- fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,4 @@ def is_same_class(obj, a_class):
 
     """function returns true if isinstance else fale"""
 
-    if isinstance(obj, a_class):
-        return True
-
-    return False
+    return type(obj) == a_class
