329ba79dab2a79adf6b4c0fca3752d8f92c73130
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 15:00:47 -0800

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""module to  check if it's instance of class or a class that inherits class"""
+
+
+def def inherits_from(obj, a_class):
+
+    """function"""
+
+    return True if isinstance(obj, a_class) else False
