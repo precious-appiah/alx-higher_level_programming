f9c477b5b7e5c27ec4fc51cc8534a83dc3526e84
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 14:32:46 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""module to check instance of an obj"""
+
+
+def is_same_class(obj, a_class):
+
+    """function returns true if isinstance else fale"""
+
+    if isinstance(obj, a_class):
+        return True
+
+    return False
