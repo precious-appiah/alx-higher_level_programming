52fca39210bd1da87de56ccacf94df9e79c5cdc7
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 14:53:48 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""module to  check if it's instance of class or a class that inherits class"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """function"""
+
+    return True if isinstance(obj, a_class) else False
