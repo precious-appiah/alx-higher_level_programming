783af23365a60a884a66feb6a799bd069ebde55c
precious-appiah <appiahprecious99@gmail.com>
2023-12-05 13:47:12 -0800

    task 3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,3 +5,9 @@
 
 
 def append_write(filename="", text=""):
+
+    """function will append data to a file """
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
