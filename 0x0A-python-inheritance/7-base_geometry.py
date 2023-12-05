911846c652d5b10f3ad95b005e3fabb30c7c1a7b
precious-appiah <appiahprecious99@gmail.com>
2023-12-04 16:12:40 -0800

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""module """
+
+
+class BaseGeometry:
+
+    """defines an empty class"""
+
+    def area(self):
+
+        """function to raise an exception"""
+
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+
+        """function to validate in"""
+
+        if isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
