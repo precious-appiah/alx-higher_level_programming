26a61a30088e3f4faefe7dc1183c182222ddbb31
precious-appiah <appiahprecious99@gmail.com>
2023-12-05 06:31:32 -0800

    task1 fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,4 +10,4 @@ def write_file(filename="", text=""):
     text is the string to be written"""
 
     with open(filename, 'w', encoding='utf-8') as f:
-        text_written=f.write(text)
+        text_written = f.write(str(text))
