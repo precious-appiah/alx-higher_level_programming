0c7377d91f5b143598abd28075907baa6c5d4d9c
precious-appiah <appiahprecious99@gmail.com>
2023-12-05 04:38:31 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+
+""" module to open file and writhe content to stdout"""
+
+
+def read_file(filename=""):
+
+    """  function that reads file 
+    an write to output"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    print("{}".format(content))
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Learning File input and output in pyhton
