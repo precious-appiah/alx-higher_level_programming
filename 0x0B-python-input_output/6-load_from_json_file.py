a98d5bd64e35c4d410b5bff18d7cef33af6c557f
precious-appiah <appiahprecious99@gmail.com>
2023-12-05 15:36:11 -0800

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""module  to convert to json content read from a file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+
+    """function to do that"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+
+        return json.dumps(f.read())
