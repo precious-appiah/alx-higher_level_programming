15faea6e5a6295e8c4779ea7bec0e29848de3ab1
precious-appiah <appiahprecious99@gmail.com>
2023-12-05 18:48:00 -0800

    task 7- fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     args = sys.argv[1:]
 
     try:
@@ -22,4 +22,4 @@ if __name__ == __main__:
 
     existing_data.extend(args)
 
-    save_to_json_file('add_item.json', existing_data)
+    save_to_json_file(existing_data, 'add_item.json')
