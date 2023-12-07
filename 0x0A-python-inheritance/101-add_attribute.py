0a956cd973efbd6da30a384319233c5cbebf9fa7
precious-appiah <appiahprecious99@gmail.com>
2023-12-06 16:30:47 -0800

    fixing

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,9 +5,7 @@
 
 
 def add_attr(obj, attr, val):
-
-    """adds new attribute"""
-
+    """adds new attribute to an object"""
     if '__dict__' in dir(obj):
         setattr(obj, attr, val)
     else:
