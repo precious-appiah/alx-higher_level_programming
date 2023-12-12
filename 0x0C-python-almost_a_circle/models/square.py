756b58347489348b0810f120a34d423f9ed70bb5
precious-appiah <appiahprecious99@gmai.com>
2023-12-11 16:31:40 -0800

    task 11wq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
 
+
+"""
+this is a module to define a square
+class square inherits from a rectactangle
+"""
+
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    """this class contanins attributes and methods of a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        
+        """function to initialize an instance of square class"""
+
+        super().__init__(size, size, x, y, id)
+
+    pass
