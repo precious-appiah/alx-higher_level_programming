295970bdf23669fdb8ab66e25133e66635950d29
angelotheman <ktwum721@gmail.com>
2023-12-12 02:10:04 +0000

    Updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
-
 """
 module to contain all that we've learnt
 """
 
 
 class Base:
-
-    """ base class for the project"""
-
+    """base class for the project"""
     __nb_objects = 0
 
     def __init__(self, id=None):
