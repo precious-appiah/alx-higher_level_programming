e35c1ee8377f4d82ce80968007d4e5061b688aac
precious-appiah <appiahprecious99@gmai.com>
2023-12-13 10:56:12 -0800

    fixing 18 eer now

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,13 +62,7 @@ class Base:
         """function to returns an instance with all
         attributes set using **dictionary"""
 
-        if cls.__name__ == "Rectangle":
-            dummy_instance = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy_instance = cls(1)
-        else:
-            raise NotImplementedError("create method not implemented \
-                    for class {}".format(cls.__name__))
+        dummy_instance = cls(1, 1)   
 
         dummy_instance.update(**dictionary)
         return dummy_instance
