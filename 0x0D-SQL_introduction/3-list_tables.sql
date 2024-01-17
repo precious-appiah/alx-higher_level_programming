9c77b8980427c070615e945e36cf6e74c4f8ecd5
precious-appiah <appiahprecious99@gmail.co>
2024-01-17 16:59:52 +0000

    task 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,9 @@
+-- show tables in a particular db
+-- SET @db_name = NULL;
+-- IF LENGTH(@argv) > 0 THEN
+--    SET @db_name := @argv;
+-- END IF;
+
+-- USE @db_name;
+SHOW TABLES;
+
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create table in db
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXIST first_table (
+    id INT,
+    name VARCHAR(255)
+);
