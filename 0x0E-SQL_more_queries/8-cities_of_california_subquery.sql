35d0e8ad6f14d61b9810207f379fb27b7ae6e0fa
precious-appiah <appiahprecious99@gmail.co>
2024-01-19 01:31:35 +0000

    TASK 8 FIX

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
---looking for an item when we only know the name 
+-- looking for an item when we only know the name 
 SELECT * FROM cities WHERE id =(
-    SELECT id FROM states where name = 'California';
+    SELECT id FROM states WHERE name = 'California';
 );
