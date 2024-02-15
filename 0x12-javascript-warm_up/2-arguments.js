0424e975a338358ac0caab545cd3abf85e59ccbb
precious-appiah <appiahprecious99@gmail.co>
2024-02-15 20:59:51 +0000

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 const { argv } = require('process');
 const argc = argv.length - 2;
 
-if (argc == 0){
+if (argc === 0) {
     console.log('No argument');
-}else if (argc == 1){
+} else if (argc === 1) {
     console.log('Argument found');
-}else {
+} else {
     console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const {argv} = require('process');
-const argc = argv.length - 2;
-if (argc == 0){
+if (!argv[2]){
     console.log('No argument');
 }else {
     console.log(`${argv[2]}`);
