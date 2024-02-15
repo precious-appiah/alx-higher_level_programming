3a594ddc694aed9c607e48f6571016812088abd2
precious-appiah <appiahprecious99@gmail.co>
2024-02-15 21:01:38 +0000

    semistandard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const {argv} = require('process');
-if (!argv[2]){
-    console.log('No argument');
-}else {
-    console.log(`${argv[2]}`);
+const { argv } = require('process');
+if (!argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(`${argv[2]}`);
 }
-
