bf9aaf7df9deafbc38e49676a671ff51de7ccb69
precious-appiah <appiahprecious99@gmail.co>
2024-02-19 04:33:33 +0000

    taske 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const n = argv[2];
-let res;
 function factorial (n) {
   if (!n || isNaN(Number(n)) || Number(n) === 0 || Number(n) === 1) {
     return 1;
