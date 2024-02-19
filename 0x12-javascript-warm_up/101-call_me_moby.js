382968e2ae01cbce21ab613f92be3bf179738aba
precious-appiah <appiahprecious99@gmail.co>
2024-02-19 05:21:03 +0000

    advanced tasks

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction){
-    let i = 0;
-    while (i < x){
-        theFunction();
-        i++;
-    }
+function callMeMoby (x, theFunction) {
+  let i = 0;
+  while (i < x) {
+    theFunction();
+    i++;
+  }
 }
 
-module.exports = {callMeMoby};
+module.exports = { callMeMoby };
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function addMeMaybe(number, theFunction){
-    number++;
-    theFunction(number);
+function addMeMaybe (number, theFunction) {
+  number++;
+  theFunction(number);
 }
-module.exports = {addMeMaybe};
+module.exports = { addMeMaybe };
