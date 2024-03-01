efa7140eadc23ea7a619c2b1f02ec0888eca8f55
precious-appiah <appiahprecious99@gmail.co>
2024-03-01 15:46:25 +0000

    task 1 fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,4 @@
 import sys
 import urllib.request
 with urllib.request.urlopen(sys.argv[1]) as response:
-    res_arr = response.headers.items()
-    for (x, y) in res_arr:
-        if x == "X-Request-Id":
-            print(y)
+    print(response.headers["X-Request-Id"])
