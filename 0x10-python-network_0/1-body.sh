4f8a0e57df4a419d9d5157b1c54fe5cbf3b95ddb
precious-appiah <appiahprecious99@gmail.co>
2024-03-01 05:31:25 +0000

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# make a get request using curl and response body with status of 200
+# make a get request using curl and get response body when status is  200
 status = curl -sI "$2" | grep -i "status"
