fbb4d9c8e0583be204a3f797932d07545317e11b
precious-appiah <appiahprecious99@gmail.co>
2024-03-01 15:54:15 +0000

    task 1 fix-fin pleasseee

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,4 +2,5 @@
 import sys
 import urllib.request
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.info().get("X-Request-Id"))
+    id = response.info().get("X-Request-Id")
+    print(id)
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from sys import argv
+from urllib.request import urlopen
+
