3848b0e73887cc0ac3219def315cdcd95427ef79
precious-appiah <appiahprecious99@gmail.co>
2024-03-12 09:32:54 +0000

    task 11- fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,23 @@
 #!/usr/bin/node
 const { argv } = require('process');
 if (argv.length <= 3) {
-    console.log(0)
+  console.log(0);
 } else {
-    let arr=[];
-    let i=2;
-    while (i<argv.length){
-        arr.push(Number(argv[i]));
-        i++;
+  const arr = [];
+  let i = 2;
+  while (i < argv.length) {
+    arr.push(Number(argv[i]));
+    i++;
+  }
+  let first = arr[0];
+  let second = arr[0];
+  for (let j = 0; j < arr.length; j++) {
+    if (first < arr[j]) {
+      second = first;
+      first = arr[j];
+    } else if (second < arr[j]) {
+      second = arr[j];
     }
-    let first = arr[0];
-    let second = arr[0];
-    for (let j = 0; j < arr.length; j++) {
-        if (first < arr[j]) {
-            second = first;
-            first = arr[j];
-        } else if (second < arr[j]) {
-            second = arr[j];
-        }
-    }
-    console.log(second);
-    return;
+  }
+  console.log(second);
 }
-
