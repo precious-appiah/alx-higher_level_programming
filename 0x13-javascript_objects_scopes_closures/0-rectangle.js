a66dc588695deee1fce1bda15450e76c3688a65e
precious-appiah <appiahprecious99@gmail.co>
2024-03-15 15:52:44 +0000

    task fix

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-class Rectancle {
+class Rectangle {
+
 }
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-class Rectancle {
+class Rectangle {
   constructor (w, h) {
     this.height = h;
     this.width = w;
   }
 }
+
+module.exports = Rectangle;
