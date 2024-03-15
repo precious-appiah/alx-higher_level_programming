d1f0e138c81d697b0117d07b03ef873e0a960c4b
precious-appiah <appiahprecious99@gmail.co>
2024-03-15 15:28:47 +0000

    task 0-2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor( w, h){
+    if (w <= 0 || h<=0){
+        this.width = null;
+        this.heigth = null
+    }
+    else{
+        this.width = w;
+        this.heigth = h;
+    }
+
+}
+}
