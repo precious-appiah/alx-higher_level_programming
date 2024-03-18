d027a9d6e294f4267b5b9320b3c83285247fa2e7
precious-appiah <appiahprecious99@gmail.co>
2024-03-18 07:36:38 +0000

    task 1 fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-exports.esrever = function (list){
-    let newArr = [];
-    let i =list.length - 1;
-    while (i >= 0){
-        newArr.push(list[i])
-        i--;
-    }
-    return newArr;
-}
+exports.esrever = function (list) {
+  const newArr = [];
+  let i = list.length - 1;
+  while (i >= 0) {
+    newArr.push(list[i]);
+    i--;
+  }
+  return newArr;
+};
