ac48adb96f0c74783588a0c07f13ce483b5dbd38
precious-appiah <appiahprecious99@gmail.co>
2024-03-26 03:11:05 +0000

    taking care of sql injections

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script to list all cities """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    conn = db.cursor()
+    conn.execute('SELECT * FROM cities;')
+    res = conn.fetchall()
+    for i in res:
+        print(i)
+    conn.close()
+    db.close()
