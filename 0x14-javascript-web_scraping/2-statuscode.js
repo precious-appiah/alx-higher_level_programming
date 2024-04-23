53ef976ed3d21ef6176315c9795e73a4d123070d
precious-appiah <appiahprecious99@gmail.co>
2024-04-23 23:59:54 +0000

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('process');
+request(argv[2], (response) => {
+  console.log('code: ', response && response.statusCode);
+});
