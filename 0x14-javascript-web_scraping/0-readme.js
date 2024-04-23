4122a2b267cb063c372bfe87ca18ce6aee137ebb
precious-appiah <appiahprecious99@gmail.co>
2024-04-23 23:18:57 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const fs = require('fs');
+
+//console.log(argv[2])
+
+fs.readFile(argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+  return;
+});
