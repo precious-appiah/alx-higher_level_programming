db8c87f35f735b41bc0eb540fb7cb1f29f626e4c
precious-appiah <appiahprecious99@gmail.co>
2024-04-24 00:18:54 +0000

    task 2 fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const { argv } = require('process');
-request(argv[2], (response) => {
-  console.log('code: ', response && response.statusCode);
+request(argv[2], (error, response) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code:',response && response.statusCode);
 });
