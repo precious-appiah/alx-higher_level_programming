6d25a87459d01656bc9cda284812323259627795
precious-appiah <appiahprecious99@gmail.co>
2024-04-25 04:45:14 +0000

    task 4 fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,9 +10,9 @@ request(`${argv[2]}`, (error, response, body) => {
   let result = JSON.parse(body);
   result = result?.results;
   let sum = 0;
-  for (const i in result) {
+  for (let i in result) {
     const comp = result[i].characters;
-    for (const x in comp) {
+    for (let x in comp) {
       if (comp[x] == 'https://swapi-api.alx-tools.com/api/people/18/') {
         sum += 1;
       }
