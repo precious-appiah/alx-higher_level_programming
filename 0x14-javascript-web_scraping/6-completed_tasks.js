5bc6aaa90ab67918e0b34823a35480bc6faf5972
precious-appiah <appiahprecious99@gmail.co>
2024-04-25 04:33:58 +0000

    task 6 draft

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/todos', (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const info = JSON.parse(body);
+  let tracker = 0;
+  let id = 0;
+  let min = 0;
+  console.log(info.length);
+  while (tracker < info.length) {
+    let sum = 0;
+    for (const i in info) {
+      if (info[i].userId == id && info[i].completed == true) {
+        sum++;
+      }
+    }
+    min = sum;
+    id++;
+    tracker++;
+  }
+  // console.log(info[id - 1].userId,":", min)
+});
