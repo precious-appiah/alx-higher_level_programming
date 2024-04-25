898c7f7754b035f30d89568a860b9d5e2883b556
precious-appiah <appiahprecious99@gmail.co>
2024-04-25 04:38:14 +0000

    task 6 draft

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const {argv} = require('process');
+const { argv } = require('process');
 const fs = require('fs');
 
-request(`${argv[2]}`,(error, response, body)=>{
-    if(error){
-        console.log(error);
-        return;
+request(`${argv[2]}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(argv[3]);
+  fs.writeFile(`${argv[3]}`, body, (error) => {
+    if (error) {
+      console.log(error);
     }
-    console.log(argv[3])
-    fs.writeFile(`${argv[3]}`,body, (error)=>{
-        if(error){
-
-            console.log('hmmmmmmmm');
-            return;
-        }
-
-    })
-
-})
-
+  });
+});
